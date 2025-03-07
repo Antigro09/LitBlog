d5a1992420fbdd6d347f0211ea84ca4a5aa8d82b
Anthony Cavero <anthonyj.cavero@gmail.com>
2025-03-07 10:35:00 -0500

    maybe fixed fontfamily?

--- litblogs/src/ClassFeed.jsx
+++ litblogs/src/ClassFeed.jsx
@@ -239,9 +239,9 @@ const TINYMCE_CONFIG = {
   menubar: false,
   plugins: [
     'advlist','typography','lists', 'link', 'image', 'charmap',
-    'searchreplace', 'code', 'fullscreen', 'table'
+    'searchreplace', 'code', 'fullscreen', 'table', 'wordcount', 'fontfamily'
   ],
-  toolbar: 'formatselect | forecolor backcolor blocks fontfamily fontsize| bold italic underline strikethrough| alignleft aligncenter alignright | bullist numlist | removeformat',
+  toolbar: 'formatselect | forecolor backcolor blocks fontfamily fontsizeselect| bold italic underline strikethrough| alignleft aligncenter alignright | bullist numlist | removeformat',
   block_formats: 'Paragraph=p; Title=h1; Heading=h2; Subheading=h3; Small Heading=h4; Blockquote=blockquote',
   forced_root_block: 'p',
   content_style: `
