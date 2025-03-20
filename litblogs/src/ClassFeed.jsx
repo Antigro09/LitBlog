cf4826ab50eeb0c94b65e1c0a1e11fc11215df1f
Antigro09 <anthonyj.cavero@gmail.com>
2025-03-20 10:28:17 -0400

    init

--- litblogs/src/ClassFeed.jsx
+++ litblogs/src/ClassFeed.jsx
@@ -5,13 +5,13 @@ import axios from 'axios';
 import EmojiPicker from 'emoji-picker-react';
 import { GiphyFetch } from '@giphy/js-fetch-api';
 import Prism from 'prismjs';
-import 'prismjs/themes/prism-tomorrow.css';  // Dark theme
+import 'prismjs/themes/prism-tomorrow.css';
 import 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-python';
 import 'prismjs/components/prism-java';
-import 'prismjs/components/prism-markup'; // For HTML
+import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-sql';
 import Loader from './components/Loader';
